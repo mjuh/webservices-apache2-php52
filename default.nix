29f20ec0c830790f0387668ce56b5dc544ce017e
Oleg Pykhalov <go.wigust@gmail.com>
2022-04-21 17:34:24 +0300

    Revert "default.nix: Fix missing mjHttpErrorPages package."
    This reverts commit 17739ad908d0ee31e08f316fd1f24b2acbe126b2, which breaks
    realpath() function:
    
    oleg@guixsd ~$ curl --resolve php52.web32.ru:80:185.84.108.18 php52.web32.ru/test.php
    bool(false)
    
    before 17739ad908d0ee31e08f316fd1f24b2acbe126b2:
    
    Deployed manually:
    
    [root@web25:~]# curl --resolve awstats.art-do.ru:8052:127.0.0.1 awstats.art-do.ru/test.php
    string(33) "/home/midzumskru/art-doru/awstats"
    
    [jenkins@jenkins:~]$ docker pull docker-registry.intr/webservices/apache2-php52:58b96f1f
    58b96f1f: Pulling from webservices/apache2-php52
    Digest: sha256:3b505ac1e614f877aa99afb28394dd01b6048a5634ca317740a6f484370f47b9
    Status: Image is up to date for docker-registry.intr/webservices/apache2-php52:58b96f1f
    docker-registry.intr/webservices/apache2-php52:58b96f1f
    
    [jenkins@jenkins:~]$ docker tag docker-registry.intr/webservices/apache2-php52:58b96f1f docker-registry.intr/webservices/apache2-php52:master
    
    [jenkins@jenkins:~]$ docker push docker-registry.intr/webservices/apache2-php52:master
    
    Revert "flake.lock: Update majordomo input."
    
    This reverts commit eab0605685db31ac20089033d22044a7a4623f37.
    
    Revert "flake.lock: Update majordomo input."
    
    This reverts commit 3262a481af965d2b157f3be9aee39b60f5350382.

--- default.nix
+++ default.nix
@@ -15,8 +15,7 @@ let
     name = "apache2-rootfs-php52";
     src = ./rootfs;
     inherit zlib curl coreutils findutils apacheHttpdmpmITK apacheHttpd
-      s6 execline php52 logger;
-    mjHttpErrorPages = mj-http-error-pages;
+      mjHttpErrorPages s6 execline php52 logger;
     postfix = sendmail;
     mjperl5Packages = mjperl5lib;
     ioncube = ioncube.v52;
@@ -35,19 +34,16 @@ pkgs.dockerTools.buildLayeredImage rec {
   tag = "latest";
   contents = [
     rootfs
-    tzdata
-    apacheHttpd
+    tzdata apacheHttpd
     locale
     sendmail
     sh
     coreutils
     libjpeg_turbo
     jpegoptim
-    (optipng.override { inherit libpng; })
+    (optipng.override{ inherit libpng ;})
     imagemagick
-    gifsicle
-    nss-certs.unbundled
-    zip
+    gifsicle nss-certs.unbundled zip
     gcc-unwrapped.lib
     glibc
     zlib
@@ -74,18 +70,18 @@ pkgs.dockerTools.buildLayeredImage rec {
       ru.majordomo.docker.exec.reload-cmd = "${apacheHttpd}/bin/httpd -d ${rootfs}/etc/httpd -k graceful";
     };
   };
-  extraCommands = ''
-    set -xe
-    ls
-    mkdir -p etc
-    mkdir -p bin
-    chmod u+w usr
-    mkdir -p usr/local
-    mkdir -p opt
-    ln -s ${php52} opt/php52
-    ln -s /bin usr/sbin
-    ln -s /bin usr/local/bin
-    mkdir tmp
-    chmod 1777 tmp
-  '';
+    extraCommands = ''
+      set -xe
+      ls
+      mkdir -p etc
+      mkdir -p bin
+      chmod u+w usr
+      mkdir -p usr/local
+      mkdir -p opt
+      ln -s ${php52} opt/php52
+      ln -s /bin usr/sbin
+      ln -s /bin usr/local/bin
+      mkdir tmp
+      chmod 1777 tmp
+    '';
 }
